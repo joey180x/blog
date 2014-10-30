90a4cfa65558056d25ef96d8a13123dfd478ea0d
Joseph De La Portilla <joey180x@gmail.com>
2014-10-30 12:00:50 -0700

    query id

--- controller/create-db.php
+++ controller/create-db.php
@@ -40,6 +40,16 @@
 		echo "Database already exists";
 	}
 
-
+	//query id for each post
+	//makes it go 1,2,3,4 ect.
+	//max length of title is 255
+	//not null means title cant be empty
+	$query = $connection->query("CREATE TABLE posts ("
+		. "id  int(11) NOT NULL AUTO_INCREMENT,"
+		. "title varchar(255) NOT NULL,"
+		. "post text NOT NULL,"
+		. "PRIMARY KEY (id))");
+	//Primary key = way tables are connected to each other
+	//every post has to have a id, title, and post text
 
 	$connection->close();
