a70ef5d3cb072033a39b460bf7a8561ceef9fbe1
Joseph De La Portilla <joey180x@gmail.com>
2014-11-03 13:16:02 -0800

    video 16 seperate parts

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
-<!DOCTYPE html>
-
-<html>
-<body>
-
 <?php
-require_once(__DIR__ . "/controller/create-db.php");
-//direct connection
+	require_once(__DIR__ . "/view/header.php");
+	require_once(__DIR__ . "/controller/create-db.php");
+	require_once(__DIR__ . "/view/footer.php");
+	//direct connection
 ?>
-
-</body>
-</html>
