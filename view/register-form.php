b588a7e4aeed3ebceb6379de3b2c4c876f25d9e5
Joseph De La Portilla <joey180x@gmail.com>
2014-12-02 12:13:38 -0800

    making the regestration file

---
+++ view/register-form.php
@@ -0,0 +1,24 @@
+<form>
+	<div>
+	<label for="email">Email: </label>
+	<input type="text" name="email" />
+	<!--sign in sheet for blog-->
+	</div>
+
+	<div>
+		<label for="username">Username: </label>
+		<input type="text" name="username">
+		<!--Username for blog-->
+	</div>
+
+	<div>
+		<label for="password">Password</label>
+		<input type="password" name="password" />
+		<!--Password for blog-->
+	</div>
+
+	<div>
+		<button type="submit">Submit </button>
+		<!--Submit button-->
+	</div>
+</form>
