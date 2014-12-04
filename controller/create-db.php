f074f1d79278dc7e61ebc1dc90e0ae854afaacd1
Joseph De La Portilla <joey180x@gmail.com>
2014-12-04 12:26:09 -0800

    created table for users - video 38

--- controller/create-db.php
+++ controller/create-db.php
@@ -12,6 +12,7 @@
 		. "title varchar(255) NOT NULL,"
 		. "post text NOT NULL,"
 		. "PRIMARY KEY (id))");
+	//creates table for posts
 	//Primary key = way tables are connected to each other
 	//every post has to have a id, title, and post text
 
@@ -19,7 +20,31 @@
 		echo "<p>Succesfully created table: posts</p>.";
 	}
 	//echos if it has been succesfully created
+
 	else{
 		echo "<p>" . $_SESSION["connection"]->error . "</p>";
 	}
+	//figuring out if query works or not
 
+	$query = $_SESSION["connection"]->query("CREATE TABLE users ("
+			. "id int(11) NOT NULL AUTO_INCREMENT," 
+			. "username varchar(30) NOT NULL,"
+			. "email varchar(50) NOT NULL,"
+			. "password char(128) NOT NULL,"
+			. "salt char(128) NOT NULL,"
+			. "PRIMARY KEY (id))");
+	//no hackers = salt
+	//user must have a username, password and 
+	//emails have to have characters and can have 50 max
+	//30 characters in a username
+	//username cant be blank
+	//increments id automatically
+	//storing value if query is sucessful (true/false)
+
+	if ($query) {
+		echo "<p>Succesfully created table: users</p>";	
+	}
+	else {
+		echo "<p>" . $_SESSION["connection"]->error . "</p>" ;
+	}
+	//creating table for users
