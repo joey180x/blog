af841064570ff48bcfebc6062907c468857819ff
Joseph De La Portilla <joey180x@gmail.com>
2014-12-16 21:41:33 -0800

    This is what I wanted graded.  This blog is way to sexy. And i still have more to add....

--- view/body.php
+++ view/body.php
@@ -6,37 +6,35 @@
 <link rel="stylesheet" type="text/css" href="css/blog.css">
 
 
-<div id="bodyrow" class="row"> 
-		<div id="left" class="col-xs-6">
+
+    <div id="bodyrow" class="row"> 
+		<div id="posts" class="col-xs-6">
 			<?php require_once(__DIR__ . "/../controller/read-posts.php"); ?>
 		</div>
-		<div id="aboutme" class="col-xs-6">
-			<h1>About Me</h1>
-			<p>Hello, my name is Joseph De La Portilla, I am a sophomore from Pasadena High School thats in The App Academy. The App Academy
-    is a four year program, that starts from when you fist enter high school, til you graduate high school. This is
-    currently my second year in The App Academy and I love it, I love learning about all these different
-    lines of code, commands, classes, id's, ect. too many to name.
+		  <div id="aboutme" class="col-xs-6">
+			 <h1>About Me</h1>
+			     <p>Hello, my name is Joseph De La Portilla, I am a sophomore from Pasadena High School thats in The App Academy. The App Academy
+                    is a four year program, that starts from when you fist enter high school, til you graduate high school. This is
+                    currently my second year in The App Academy and I love it, I love learning about all these different
+                    lines of code, commands, classes, id's, ect. too many to name.
 
- I'm 15 years old play baseball, video games, love learning about code, and putting it in to play in
-    my own customizable way. I play baseball here at Pasadena High and it's fun and a good workout. I've
-    been playing baseball since I was 5 and I never had had this much excersize in my life. Ever since I
-    heard about The App Academy I wanted to join as soon as I got the chance to.The App Academy has quite a bit
-    of perks, from getting to go during school days to go see people code for work, to getting to go to
-    Microsoft and be part of a Hack-a-Thon. It's the best class of the day(by far) and everything about it 
-    is just great. When I get older I hope to be able to code for a living and making a great life out of it.
-    To help shape the future, just like all of the great teachers I've had, a big thank you to them for helping
-    me so much. This website I'm making right now you're reading this on, would not have been possible...without
-    The App Academy.
-  </p>
+                    I'm 15 years old play baseball, video games, love learning about code, and putting it in to play in
+                    my own customizable way. I play baseball here at Pasadena High and it's fun and a good workout. I've
+                    been playing baseball since I was 5 and I never had had this much excersize in my life. Ever since I
+                    heard about The App Academy I wanted to join as soon as I got the chance to.The App Academy has quite a bit
+                    of perks, from getting to go during school days to go see people code for work, to getting to go to
+                    Microsoft and be part of a Hack-a-Thon. It's the best class of the day(by far) and everything about it 
+                    is just great. When I get older I hope to be able to code for a living and making a great life out of it.
+                    To help shape the future, just like all of the great teachers I've had, a big thank you to them for helping
+                    me so much. This website I'm making right now you're reading this on, would not have been possible...without
+                    The App Academy.
+            </p>
+            </div>
 		</div>
-	</div>
+	   </div>
  
-
-
-
- <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
- <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>	
- <script type="text/javascript" src="java/javascript.js"></script>
-	</body>
-</html>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>	
+<script type="text/javascript" src="java/javascript.js"></script>
+	
--- view/header.php
+++ view/header.php
@@ -6,35 +6,34 @@
 <link rel="stylesheet" type="text/css" href="css/blog.css">
 <html>
 	<head>
-		<meta charset= "UTF-8">
 		<title>Joseph's Blog</title>
 	</head>
 <body>
 
-	<div class= "container">
-		<div class="col-xs-9">
-			<h1>Joseph's Blog</h1>
-			
+	<div id="header">
+		<div class="col-xs-4">
+			<h1>Welcome to my Blog</h1>
+				<div>
+				<a href="/blog/controller/logout-user.php" class="btn btn-primary btn-s active" role="button">Logout</a>
+				<a href="/blog/login.php" class="btn btn-primary btn-s active" role="button">Login</a>
+				<a href="/blog/register.php" class="btn btn-primary btn-s active" role="button">Register</a>
+				</div>
 				<div id="social-media">
 					<a href="https://www.Facebook.com/">
 						<img src="/blog/images/Facebookicon.png" alt="Facebook" height="50px" width="50px">
 					</a>
-					<a href="http://www.Instagram.com/">
+					<a href="http://www.Instagram.com/joey180x">
 						<img src="/blog/images/Instagram.png" alt="Facebook" height="50px" width="50px">
 					</a>
-					<a href="http://www.Twitter.com/">
+					<a href="http://www.Twitter.com/xjoey180x">
 						<img src="/blog/images/Twittericon.png" alt="Facebook" height="50px" width="50px">
 					</a>
-					<hr>
+					
 				</div>
 			</div>
-			<a href="/blog/controller/logout-user.php" class="btn btn-primary btn-lg active" role="button">Logout</a>
-			<a href="/blog/login.php" class="btn btn-primary btn-lg active" role="button">Login</a>
-			<a href="/blog/register.php" class="btn btn-primary btn-lg active" role="button">Register</a>
 		</div>
+			
  
-
-
  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
  <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>	
